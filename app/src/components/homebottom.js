2a04a27f119e52ff0534559eb24a147bf1d43c70
Jiaowenhui <523947655@qq.com>
2017-04-06 17:49:10 +0800

    查看大图样式补传

---
+++ app/src/components/homebottom.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Button,Segment } from 'semantic-ui-react';
+import {clickTab} from '../actions/index.js';
+import Page0 from './home.js';
+import Page1 from './community.js';
+//import Page2 from './test';
+import Page2 from './shopping/index.js';
+import Page3 from './myprofile.js';
+import '../../public/css/index.css';
+
+export class Page extends React.Component {
+    componentWillMount() {
+
+    }
+
+    onClickTab(curtabindex) {
+        this.props.dispatch(clickTab({curtabindex: curtabindex}));
+    }
+
+    render() {
+        let pagesz = [];
+        pagesz.push(<Page0 key="page0" {...this.props} />);
+        pagesz.push(<Page1 key="page1"  {...this.props} />);
+        pagesz.push(<Page2 key="page2"  {...this.props} />);
+        pagesz.push(<Page3 key="page3"  {...this.props} />);
+        let pagewamp = {
+            height: window.height,
+        }
+        let btnsz = [
+            {
+                title: '首页',
+                imgnormal: 'img/bottom2.png',
+                imghov: 'img/bottom1.png',
+            },
+            {
+                title: '圈子',
+                imgnormal: 'img/bottom4.png',
+                imghov: 'img/bottom3.png',
+            },
+            {
+                title: '商城',
+                imgnormal: 'img/bottom6.png',
+                imghov: 'img/bottom5.png',
+            },
+            {
+                title: '我的',
+                imgnormal: 'img/bottom8.png',
+                imghov: 'img/bottom7.png',
+            },
+        ];
+        let btncosz = [];
+        btnsz.forEach((obj,index)=> {
+            if (index === this.props.curtabindex) {
+                btncosz.push(<Button key={'btn'+index} onClick={this.onClickTab.bind(this,index)} className="action">
+                    <img src={btnsz[index].imghov}/>
+                    <span>{btnsz[index].title}</span>
+                </Button>);
+            }
+            else {
+                btncosz.push(<Button key={'btn'+index} onClick={this.onClickTab.bind(this,index)}>
+                    <img src={btnsz[index].imgnormal}/>
+                    <span>{btnsz[index].title}</span>
+                </Button>);
+            }
+        });
+        return (
+            <div className="homeBottom">
+                {btncosz}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = ({app}) => {
+    return app;
+};
+
+Page = connect(mapStateToProps)(Page);
+export default Page;
+
+
