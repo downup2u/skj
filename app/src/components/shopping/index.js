8fddc04bd5b47ece95d878414bde1ad586cebd27
Jiaowenhui <523947655@qq.com>
2017-04-06 17:46:40 +0800

    商城首页样式调整

--- app/src/components/shopping/index.js
+++ app/src/components/shopping/index.js
@@ -26,69 +26,71 @@ let Page = (props) => {
                 <Input placeholder="请输入关键字" value="净水器"/>
                 <img src="img/shopping/10.png"/>
             </div>
-            <div className="shoppingBanner">
-                <Swiper
-                    swiperOptions={{slidesPerView: 'auto'}}
-                    {...swiperOptions}>
-                    {_.map(props.shop.banner1, (banner)=>{
-                        return (
-                            <Slide className="Demo-swiper__slide">
-                                <img src={banner.url}/>
-                            </Slide>
-                        )
-                    })}
-                </Swiper>
-            </div>
-            <div className="listTitle">
-                <img src="img/shopping/1.png"/>
-                <span>家用套餐特惠价活动进行中</span>
-            </div>
-            <div className="shoppingBanner2">
-                <img src="img/shopping/2.png" onClick={()=>{onClickPage('/shoppingpackage')}} />
-                <img src="img/shopping/3.png" onClick={()=>{onClickPage('/shoppingpackage')}}/>
-            </div>
-            <div className="shoppingBannerLnk">
-            <span onClick={()=>{onClickPage('/shoppingprolist')}}>
-                <img src='img/shopping/4.png'/>
-                净水系统
-            </span>
-            <span onClick={()=>{onClickPage('/shoppingprolist')}}>
-                <img src='img/shopping/5.png'/>
-                卫浴系统
-            </span>
-            <span onClick={()=>{onClickPage('/shoppingprolist')}}>
-                <img src='img/shopping/6.png'/>
-                管道系统
-            </span>
-            <span onClick={()=>{onClickPage('/shoppingprolist')}}>
-                <img src='img/shopping/7.png'/>
-                品质生活
-            </span>
-            </div>
-            {_.map(props.shop.prolist, (list, typeid)=>{
-                return (
-                    <div>
-                        <div className="listTitle2" onClick={()=>{onClickPage(`/shoppingprolist/${typeid}`)}}>
-                            <span>{props.shop.protype[typeid].name}</span>
-                            <span>更多 <Icon name="angle right"/></span>
+            <div className="shoppingBody">
+                <div className="shoppingBanner">
+                    <Swiper
+                        swiperOptions={{slidesPerView: 'auto'}}
+                        {...swiperOptions}>
+                        {_.map(props.shop.banner1, (banner)=>{
+                            return (
+                                <Slide className="Demo-swiper__slide">
+                                    <img src={banner.url}/>
+                                </Slide>
+                            )
+                        })}
+                    </Swiper>
+                </div>
+                <div className="listTitle">
+                    <img src="img/shopping/1.png"/>
+                    <span>家用套餐特惠价活动进行中</span>
+                </div>
+                <div className="shoppingBanner2">
+                    <img src="img/shopping/2.png" onClick={()=>{onClickPage('/shoppingpackage')}} />
+                    <img src="img/shopping/3.png" onClick={()=>{onClickPage('/shoppingpackage')}}/>
+                </div>
+                <div className="shoppingBannerLnk">
+                <span onClick={()=>{onClickPage('/shoppingprolist')}}>
+                    <img src='img/shopping/4.png'/>
+                    净水系统
+                </span>
+                <span onClick={()=>{onClickPage('/shoppingprolist')}}>
+                    <img src='img/shopping/5.png'/>
+                    卫浴系统
+                </span>
+                <span onClick={()=>{onClickPage('/shoppingprolist')}}>
+                    <img src='img/shopping/6.png'/>
+                    管道系统
+                </span>
+                <span onClick={()=>{onClickPage('/shoppingprolist')}}>
+                    <img src='img/shopping/7.png'/>
+                    品质生活
+                </span>
+                </div>
+                {_.map(props.shop.prolist, (list, typeid)=>{
+                    return (
+                        <div>
+                            <div className="listTitle2" onClick={()=>{onClickPage(`/shoppingprolist/${typeid}`)}}>
+                                <span>{props.shop.protype[typeid].name}</span>
+                                <span>更多 <Icon name="angle right"/></span>
+                            </div>
+                            <div className="proList">
+                                {_.map(list, (pro)=>{
+                                    return (
+                                        <div className="li" onClick={()=>{onClickPage(`/shoppingproinfo/${pro.proid}`)}}>
+                                            <img src="img/shopping/8.png"/>
+                                            <span className="name">{pro.name}</span>
+                                            <span className="price">
+                                                <span>{pro.price}</span>
+                                                <img src={pro.avatar} />
+                                            </span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
                         </div>
-                        <div className="proList">
-                            {_.map(list, (pro)=>{
-                                return (
-                                    <div className="li" onClick={()=>{onClickPage(`/shoppingproinfo/${pro.proid}`)}}>
-                                        <img src="img/shopping/8.png"/>
-                                        <span className="name">{pro.name}</span>
-                                        <span className="price">
-                                            <span>{pro.price}</span>
-                                            <img src={pro.avatar} />
-                                        </span>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     );
 }
