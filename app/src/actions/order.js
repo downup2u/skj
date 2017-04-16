47e6877347745c9ba8d8d3c709660ec1751b97e2
Jiaowenhui <523947655@qq.com>
2017-04-16 21:12:09 +0800

    分离order的action

---
+++ app/src/actions/order.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wangxiaoqing on 2017/4/7.
+ */
+import { createAction } from 'redux-act';
+
+//设置订单列表页面筛选参数
+export const myOrderList_filler_set = createAction('myOrderList_filler_set');
