c4e9f89d5baa1d34f7dfc6d85fc089814cc003ec
wangxiaoqing <xiaoqingwang@126.com>
2017-06-26 01:48:44 +0800

    +异常捕获

--- app/src/sagas/payflow.js
+++ app/src/sagas/payflow.js
@@ -30,38 +30,44 @@ export function* payflow() {
 
     yield takeEvery(`${payorder_result}`, function*(action) {
           let {payload:result} = action;
-          console.log(`payorder_result:${JSON.stringify(result)}`);
-          const {orderinfo} = result;
-          let orderdoc = {
-             out_trade_no: orderinfo._id,
-             subject: orderinfo.ordertitle || '商品名称',
-             body: orderinfo.orderdetail|| '商品详情',
-             total_fee: orderinfo.realprice
-           };
-           if(orderinfo.paytype === 'weixin'){
-             orderdoc.total_fee = orderinfo.realprice*100;
-           }
-          yield put(getpaysign_request({
-              paytype:orderinfo.paytype,
-              paypage:'orderdetailpage',
-              orderdoc:orderdoc,
-          }));
-          const { response, timeout } = yield race({
-             response: take(`${getpaysign_result}`),
-             timeout: call(delay, 10000)
-          });
+          try{
+            console.log(`payorder_result:${JSON.stringify(result)}`);
+            const {orderinfo} = result;
+            let orderdoc = {
+               out_trade_no: orderinfo._id,
+               subject: orderinfo.ordertitle || '商品名称',
+               body: orderinfo.orderdetail|| '商品详情',
+               total_fee: orderinfo.realprice
+             };
+             if(orderinfo.paytype === 'weixin'){
+               orderdoc.total_fee = orderinfo.realprice*100;
+             }
+            yield put(getpaysign_request({
+                paytype:orderinfo.paytype,
+                paypage:'orderdetailpage',
+                orderdoc:orderdoc,
+            }));
+            const { response, timeout } = yield race({
+               response: take(`${getpaysign_result}`),
+               timeout: call(delay, 10000)
+            });
 
-          console.log(`response===>${JSON.stringify(response)}`);
+            console.log(`response===>${JSON.stringify(response)}`);
 
-          let { payload:paysign } =  response;
-          if(!!paysign){//失败情况下，发送paysign 为NULL
-            let payresult = yield call(takepay,paysign,orderinfo);
-            console.log(`payresult:${JSON.stringify(payresult)},orderinfo.triptype:${orderinfo.triptype}`);
-            if(orderinfo.paytype === 'leftbalance'){
-              //payresult为orderinfo
-              yield put(myorderupdateone(payresult));
+            let { payload:paysign } =  response;
+            if(!!paysign){//失败情况下，发送paysign 为NULL
+              let payresult = yield call(takepay,paysign,orderinfo);
+              console.log(`payresult:${JSON.stringify(payresult)},orderinfo.triptype:${orderinfo.triptype}`);
+              if(orderinfo.paytype === 'leftbalance'){
+                //payresult为orderinfo
+                yield put(myorderupdateone(payresult));
+              }
             }
           }
+          catch(e){
+            
+          }
+
     });
 
 
