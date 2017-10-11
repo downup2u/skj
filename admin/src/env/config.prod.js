847ad8d297e6ff04f5e6b3173154919f8e831759
wangxiaoqing <xiaoqingwang@126.com>
2017-10-11 13:53:27 +0800

    更新

--- admin/src/env/config.prod.js
+++ admin/src/env/config.prod.js
@@ -1,4 +1,4 @@
-let serverurl = 'http://shuizhihe.com28.cn:3100';
+let serverurl = 'http://shuizhihe.com28.cn:3101';
 export default {
     restserverurl:serverurl +'/adminapi',
     serverurl:serverurl
