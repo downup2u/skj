298f620879b34c5e80a0c611222ccc6af720a395
wangxiaoqing <xiaoqingwang@126.com>
2017-11-08 16:40:47 +0800

    ===

--- deploy/srv/src/devicetcp/devicedatadb.js
+++ deploy/srv/src/devicetcp/devicedatadb.js
@@ -160,18 +160,21 @@ const updatedevice =(realtimedata)=>{
 
       console.log(`devicedata==>${JSON.stringify(devicedata)}`)
       console.log(`realtimedata==>${JSON.stringify(realtimedata)}`)
+      // 首先取出systemtable表cleanCount记录中的Lx字段值赋给变量Lx，
+      // 共净化值=每个时间点的净水总流量值-初始净化值
+      const lr = cu_j - cleanCount.fv_l0;
 
       const updateddata = {detailvollist,detaildaylist,lr,cleanCount,cu_y,cu_j,realtimedata:realtimedata._id};
 
       console.log(`updateddata==>${JSON.stringify(updateddata)}`)
-      // 首先取出systemtable表cleanCount记录中的Lx字段值赋给变量Lx，
-      // 共净化值=每个时间点的净水总流量值-初始净化值
-      const lr = cu_j - cleanCount.fv_l0;
+
       dbModel.findByIdAndUpdate(devicedata._id,{
         $set:updateddata
       }, {new: true},
         (err, result)=> {
-        PubSub.publish(`device.${realtimedata.deviceid}`,realtimedata);
+          console.log(`dbModel result==>${JSON.stringify(result)}`)
+
+          PubSub.publish(`device.${realtimedata.deviceid}`,realtimedata);
       });
     }
   });
