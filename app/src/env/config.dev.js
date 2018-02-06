43560cbdf0fc193347e9017e8ec4b0ee1685f150
wangxiaoqing <xiaoqingwang@126.com>
2018-02-06 16:01:58 +0800

    v 1.3.3

--- app/src/env/config.dev.js
+++ app/src/env/config.dev.js
@@ -1,8 +1,8 @@
-let islocalhost = true;
+let islocalhost = false;
 let config = {
   serverurl:islocalhost?'http://localhost:3100':'http://skj.com28.cn',//'http://localhost:3100'
   requesttimeout:15000,
-  appversion : "1.0",
+  appversion : "1.3.3",
   appurl : "http://www.baidu.com",
   refreshserverdateinterval:60000,//1 minute
   warningpercentdefault : 0.95,//黄色提示百分比
--- app/src/env/config.prod.js
+++ app/src/env/config.prod.js
@@ -1,7 +1,7 @@
 let config = {
   serverurl:'http://skj.com28.cn',//'http://localhost:3100',//'http://vpn.czjcd.com:3100',//
   requesttimeout:15000,
-  appversion : 1.0,
+  appversion : "1.3.3",
   appurl : "http://www.baidu.com",
   refreshserverdateinterval:60000,//1 minute
   warningpercentdefault : 0.95,//黄色提示百分比
