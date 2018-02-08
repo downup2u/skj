166c422d15fe486ee1666d2c97fc811ace135854
wangxiaoqing <xiaoqingwang@126.com>
2018-02-08 14:04:22 +0800

    看看修改的部分，打印出来是5，显示的是05

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -137,13 +137,15 @@ export class ConfirmDom extends Component{
         const tpname = datatp==="vol"?"流量":"天数";
 
         if(datatp==="vol"){
+           let valuehomeconfirmday = parseInt(Math.ceil(homeconfirmday/15));
+           console.log(`valuehomeconfirmday==>${valuehomeconfirmday}`);
             return (
                 <div className="setoneform volsetoneform">
                     <div>
                         <span>净水器的使<br/>用天数</span>
                         <div className="numbrinput">
                             <span onClick={this.click_setoneinput.bind(this, "del")}> - </span>
-                            <input type="number" value={Math.ceil(homeconfirmday/15)} onChange={(e)=>{ this.props.dispatch(set_homeconfirmday(e.target.value*15)); }} />
+                            <input type="number" value={valuehomeconfirmday} onChange={(e)=>{ this.props.dispatch(set_homeconfirmday(e.target.value*15)); }} />
                             <span onClick={this.click_setoneinput.bind(this, "add")}> + </span>
                             <b>＊15＝{homeconfirmday}升</b>
                         </div>
@@ -160,13 +162,15 @@ export class ConfirmDom extends Component{
                 </div>
             )
         }else{
+            let valuehomeconfirmday = parseInt(homeconfirmday);
+            console.log(`valuehomeconfirmday==>${valuehomeconfirmday}`);
             return (
                 <div className="setoneform">
                     <div>
                         <span>净水器的使用天数</span>
                         <div className="numbrinput">
                             <span onClick={this.click_setoneinput.bind(this, "del")}> - </span>
-                            <input type="number" value={homeconfirmday} onChange={(e)=>{ this.props.dispatch(set_homeconfirmday(e.target.value)); }} />
+                            <input type="number" value={valuehomeconfirmday} onChange={(e)=>{ this.props.dispatch(set_homeconfirmday(e.target.value)); }} />
                             <span onClick={this.click_setoneinput.bind(this, "add")}> + </span>
                             <b style={{textAlign: "left",textIndent: "5px", lineHeight : "30px", width: "26px"}}></b>
                         </div>
